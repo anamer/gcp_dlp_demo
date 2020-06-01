323edc1c714e57d2d6abd4764c7f6a7b26dc74f5
Assaf Namer <assaf.namer@gmail.com>
2020-06-01 15:06:55 -0700

    Add files via upload
    Simple bash script that uses the deid.py from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/dlp/deid.py to deidentifying sensitive data.

---
+++ redact_my_pii_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Redact pii data from csv file, keep CC and role only
+filename='my_pii_data.csv'
+output_file='mi_pii_data_redacted.csv'
+rm $output_file 2> /dev/null
+while read line; do
+# reading each line
+python deid.py deid_mask --info_types FIRST_NAME LAST_NAME EMAIL_ADDRESS DATE PHONE_NUMBER DOMAIN_NAME  LOCATION IBAN_CODE  ORGANIZATION_NAME  -n 12 -m '*' assafproject-2 "$line" >> $output_file
+done < $filename
